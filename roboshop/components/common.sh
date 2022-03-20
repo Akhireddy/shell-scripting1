f2b3d32e3af63020a136d711941036bb2bf8865e
Akhireddy <akhir1989@gmail.com>
2022-03-20 23:37:53 +0800

    updated script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-USER=$(id -u)
-if [ $USER -ne 0 ]; then
-   echo -e "\e[31m you should be root user\e[0m"
-   exit 1
-   fi
-
-STAT_CHK() {
-  if [ $? -ne 0 ]; then
-    echo -e "\e[31m Failed \e[0m"
-  else
-    echo -e "\e[32m Success \e[0m"
-    exit 1
-  fi
-}
-
-Print() {
-  echo -e "\n ============= $1 ==============="
-  echo -e "\e[36m $1 \e[0m"
-}
-
 LOG_FILE=/tmp/logfile
 rm -rf $LOG_FILE
+
+USER=$(id -u)
+if [ $USER -ne 0 ]; then
+   echo -e "\e[1;31m you should be in root\e[0m"
+  exit 1
+fi
+
+Print() {
+   echo -e "\n -------------------- $1 ------------ "
+   echo -e "\e[1;36m $1 \e[0m"
+}
+
+STAT_CHK() {
+if [ $? -eq 0 ]; then
+   echo -e "\e[1;32m  Succes\e[0m"
+else
+   echo -e "\e[1;32m Failed\e[0m"
+   exit 1
+fi
+}
+
 
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,10 +2,30 @@
 
 source components/common.sh
 
-Print "install nginx"
-sudo yum install nginx -y
+Print "Installing nginx"
+yum install nginx -y &>>$LOG_FILE
+STAT_CHK $? "nginx installation"
+
+Print "Download nginx repo"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+STAT_CHK $? "down load nginx repo"
+
+Print "Deploy in Nginx Default Location"
+rm -rf /usr/share/nginx/html/*
 STAT_CHK $?
 
-Print "start nginx services"
-systemctl enable nginx && systemctl start nginx
+Print "Deploy"
+cd /usr/share/nginx/html &>>$LOG_FILE  && unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE
+STAT_CHK $?
+
+Print "copy"
+
+mv static/* . &>>LOG_FILE && rm -rf frontend-master static README.md &&>$LOG_FILE
+
+Print "move localhost config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHK $?
+
+Print "restart nginx"
+systemctl restart nginx
 STAT_CHK $?
